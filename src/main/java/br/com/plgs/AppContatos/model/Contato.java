106301a4d1cd36bff6de124483eb970d8e75f16e
PedroLGS <pedro.sampaio01@fatec.sp.gov.br>
2024-06-24 15:54:11 -0300

    Criando as entidades

---
+++ src/main/java/br/com/plgs/AppContatos/model/Contato.java
@@ -0,0 +1,99 @@
+package br.com.plgs.AppContatos.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_contatos")
+public class Contato {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private Integer tipoContato;
+	
+	@Column(nullable = false)
+	private String contato;
+	
+	@ManyToOne
+	@JoinColumn(name = "pessoa_id", referencedColumnName = "id")
+	private Pessoa pessoa;
+	
+	public Contato() { }
+	
+	public Contato(Long id, Integer tipoContato, String contato, Pessoa pessoa) {
+		this.id = id;
+		this.tipoContato = tipoContato;
+		this.contato = contato;
+		this.pessoa = pessoa;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	// 0 = telefone 1 = celular
+	public Integer getTipoContato() {
+		return tipoContato;
+	}
+
+	// 0 = telefone 1 = celular
+	public void setTipoContato(Integer tipoContato) {
+		this.tipoContato = tipoContato;
+	}
+
+	public String getContato() {
+		return contato;
+	}
+
+	public void setContato(String contato) {
+		this.contato = contato;
+	}	
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contato other = (Contato) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	@Override
+	public String toString() {		
+		return "[id = " 			+ this.id 			+ ", " +
+			   "[tipoContato = " 	+ this.tipoContato 	+ ", " +
+			   "[Contato = " 		+ this.contato 		+ "]";
+	}
+		
+}
---
+++ src/main/java/br/com/plgs/AppContatos/model/Pessoa.java
@@ -0,0 +1,137 @@
+package br.com.plgs.AppContatos.model;
+
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_pessoa")
+public class Pessoa {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = true)
+	private String endereco;
+	
+	@Column(nullable = true, length = 9)
+	private String cep;
+	
+	@Column(nullable = true)
+	private String cidade;
+	
+	@Column(nullable = true, length = 2)
+	private String uf;
+	
+	@OneToMany
+	@JoinColumn(name = "contatos_id", referencedColumnName = "id")
+	private List<Contato> contatos;
+	
+	public Pessoa() { }
+	
+	public Pessoa(Long id, String nome, String endereco, String cep, String cidade, String uf) {
+		this.id = id;
+		this.nome = nome;
+		this.endereco = endereco;
+		this.cep = cep;
+		this.cidade = cidade;
+		this.uf = uf;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}	
+
+	public List<Contato> getContatos() {
+		return contatos;
+	}
+
+	public void setContatos(List<Contato> contatos) {
+		this.contatos = contatos;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	@Override
+	public String toString() {
+		return "[id = " 		+ this.id 		+ ", " +
+			   "[Nome = " 		+ this.nome 	+ ", " +
+			   "[Endereco = " 	+ this.endereco + ", " +
+			   "[Cep = " 		+ this.cep 		+ ", " +
+			   "[Cidade = " 	+ this.cidade 	+ ", " +
+			   "[Uf = " 		+ this.uf 		+ "]";
+		
+	}
+		
+}
